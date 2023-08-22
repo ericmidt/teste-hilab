e71fa3810efcca2e1e9e922b2e6983bf9291c358
Eric Schmidt <ericschmidt49@gmail.com>
2023-08-21 22:26:59 -0300

    Add comments to notificacoes_gripe.py

--- notificacoes_sindrome_gripal/notificacoes_gripe.py
+++ notificacoes_sindrome_gripal/notificacoes_gripe.py
@@ -6,7 +6,7 @@ import calendar
 class NotificacoesProcessor:
 
     def __init__(self, db_params):
-        # API credentials
+        # Credenciais para acessar a API
         self.url = "https://elasticsearch-saps.saude.gov.br/desc-esus-notifica-estado-pr/_search"
         self.username = "user-public-notificacoes"
         self.password = "Za4qNXdyQNSa9YaA"
@@ -14,12 +14,13 @@ class NotificacoesProcessor:
         self.session = requests.Session()
         self.session.auth = (self.username, self.password)
 
+    # Busca e processa os dados
     def fetch_and_processs_data(self,start_date, end_date):
-        # Connect to the PostgreSQL database
+        # Realiza a conexão ao banco de dados
         conn = psycopg2.connect(**db_params)
         cursor = conn.cursor()
 
-        # Create table
+        # Cria uma tabela
         cursor.execute("""CREATE TABLE IF NOT EXISTS Notificacoes_sindrome_gripal_parana(
                                 id VARCHAR(255),
                                 timestamp TIMESTAMP,
@@ -40,29 +41,29 @@ class NotificacoesProcessor:
                                 profissionalSeguranca VARCHAR(255)
                         );""")
 
-        # Define the date range
+        # Define a data de início para a busca dos dados e a data final. Não há registros antes de 01/11/2022
         start_date = datetime(2022, 11, 1)
         end_date = datetime.now()
 
-        # Loop through each year and month
+        # Itera por cada ano e mês
         current_date = start_date
         while current_date <= end_date:
-            # Calculate the last day of the current month
+            # Calcula o último dia de cada mês
             last_day = calendar.monthrange(current_date.year, current_date.month)[1]
 
-            # Define the query parameters for the search
+            # Define os parâmetros para a busca
             query_params = {
                 "q": f"@timestamp:[{current_date.strftime('%Y-%m-%dT00:00:00.000Z')} TO {current_date.replace(day=last_day).strftime('%Y-%m-%dT23:59:59.999Z')}]",
-                "size": 10000  # Specify the batch size
+                "size": 10000  # Quantidade de hits a serem buscados (10 mil é o máximo da API)
             }
 
-            # Make a GET request to the API URL with query parameters
+            # Realiza um GET request para o URL da API
             response = self.session.get(self.url, params=query_params)
 
             if response.status_code == 200:
-                # Parse the JSON response
                 data = response.json()
-                # Process the hits as needed
+                # Hit é a denominação da API para uma entrada no banco, esse loop itera 
+                # por cada um e busca informações específicas
                 hits = data.get("hits", {}).get("hits", [])
                 for hit in hits:
                     source = hit.get("_source", {})
@@ -76,14 +77,16 @@ class NotificacoesProcessor:
                     for key in keys_to_retrieve:
                         value = source.get(key)
                         if value is None:
+                            # Insere um valor específico caso alguma data seja null
                             if key in ["@timestamp", "dataInicioSintomas", "dataPrimeiraDose", "dataSegundaDose", "dataNotificacao"]:
                                 selected_data.append("1970-01-01 00:00:00")
+                            # Insere N/A caso algum valor seja null
                             else:
                                 selected_data.append("N/A")
                         else:
                             selected_data.append(value)
 
-                    # Insert data into PostgreSQL database
+                    # Insere os dados no banco de dados local PostgreSQL
                     insert_query = "INSERT INTO Notificacoes_sindrome_gripal_parana VALUES (%s)" % ','.join(['%s'] * len(selected_data))
                     cursor.execute(insert_query, selected_data)
 
@@ -93,10 +96,10 @@ class NotificacoesProcessor:
             else:
                 print(f"Error in request for {current_date}: {response.status_code}")
 
-            # Move to the next month
+            # Move a busca para o próximo mês
             current_date = (current_date.replace(day=1) + timedelta(days=32)).replace(day=1)
 
-        # Close the database connection
+        # Encerra a conexão com o banco de dados
         cursor.close()
         conn.close()
 
@@ -104,7 +107,7 @@ class NotificacoesProcessor:
 
 if __name__ == "__main__":
 
-    # PostgreSQL database credentials
+    # Credenciais para conexão com o banco de dados PostgreSQL
     db_params = {
         "dbname": "postgres",
         "user": "postgres",
